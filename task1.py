9c8aacd66fe2a70ac925a44ac9928da4abd99d5f
arina909 <65169074+arina909@users.noreply.github.com>
2020-12-17 21:28:12 +0300

    added task 1 with new variable names

--- task1.py
+++ task1.py
@@ -1,14 +1,13 @@
-s = input()
-l = s.split(':')
-t = float('.'.join(l))
-eq=90/(12-6)/60
+mytime = input()
+time = float('.'.join(mytime.split(':')))
+eqiv = 90/(12-6)/60
 # find how many minutes in one sun angle
-if t > 6.00 and t < 18.00:
-    t1 = t-6.00
+if time > 6.00 and time < 18.00:
+    reltime = time-6.00
     # start counting from 6.00
-    m = t1 // 1 * 60 + t1 % 1 * 100
+    minutes = reltime // 1 * 60 + reltime % 1 * 100
     # convert hours into minutes
-    sun_angel = m*eq
+    sun_angel = minutes*eqiv
     print('%.2f' % sun_angel)
 else:
     print('I don\'t see the sun!')
