6c1c28345bfd34e942ae8712f8246ed63dc097e5
arina909 <65169074+arina909@users.noreply.github.com>
2020-12-24 15:54:18 +0300

    added task 2 with naming

--- task2.py
+++ task2.py
@@ -1,17 +1,17 @@
-s1 = [input()]
-s2 = [input()]
-i = ["dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+drone1 = [input()]
+drone2 = [input()]
+couples = ["dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
    "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"]
-b = i[0].split('-')
-c = []
-for n in range(1, len(i)):
-    k=i[n].split('-')
-    if k[0] in b or k[1] in b:
-        b+=k
+company1 = couples[0].split('-')
+company2 = []
+# create certain list for certain company
+for n in range(1, len(couples)):
+    one_couple=couples[n].split('-')
+    if one_couple[0] in company1 or one_couple[1] in company1:
+        company1+=one_couple
     else:
-        c+=k
-# create certain list for certain group
-if set(s1).issubset(b) and set(s2).issubset(b) or set(s1).issubset(c) and set(s2).issubset(c):
+        company2+=one_couple
+if set(drone1).issubset(company1) and set(drone2).issubset(company1) or set(drone1).issubset(company2) and set(drone2).issubset(company2):
     print('True')
 else:
     print('False')
